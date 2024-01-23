387c1ff3754aa316d8eafe9fed81ab80ac5d26a4
RomanMalitskyi <chebureck77@gmail.com>
2024-01-23 13:15:33 +0200

    Add menu scene. Update existed scenes

---
+++ Assets/Scripts/SceneInstallers/MenuInstaller.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Zenject;
+
+namespace ChebDoorStudio.SceneInstallers
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+
+        public override void Start()
+        {
+        }
+    }
+}
