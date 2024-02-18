1ba126123edb692731e5cd85b389bd398a43d150
DanilVasilkovskyi <44950910+Tapochki@users.noreply.github.com>
2024-02-18 23:10:29 +0100

    try new enemy model, start level system

---
+++ Assets/Scripts/Gameplay/Objects/LevelComponent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace TandC.Gameplay 
+{
+    public class LevelComponent : MonoBehaviour
+    {
+        private int _currentLevel;
+
+        private int _currentXp;
+        private int _xpForNextLevel;
+    }
+}
+
+
